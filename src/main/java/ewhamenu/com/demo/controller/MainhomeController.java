480568c4bea91c0f151cf70f089ecc019f6d549f
Serin <tpflsdlf@gmail.com>
2021-04-04 01:20:32 +0900

    first commitment - main page html (not completed) - main page controller (not completed)

---
+++ src/main/java/ewhamenu/com/demo/controller/MainhomeController.java
@@ -0,0 +1,35 @@
+package ewhamenu.com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainhomeController {
+
+    @GetMapping("/")
+    public String mainhome(){
+        return "mainhome";
+    }
+
+//    @ResponseBody
+//    public String mainhome(@RequestParam("name") String name){
+//        Homeclass home = new Homeclass();
+//        home.setName(name);
+//        return "mainhome";
+//    }
+//
+//
+//    public class Homeclass{
+//        private String name;
+//
+//        public String getName(){
+//            return name;
+//        }
+//        public void setName(String name){
+//            this.name = name;
+//        }
+//    }
+
+}

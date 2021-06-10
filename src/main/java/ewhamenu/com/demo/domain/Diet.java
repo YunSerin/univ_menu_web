4385cbe950cf34919919b5c7b6dd6ece605a4bec
Serin <tpflsdlf@gmail.com>
2021-06-10 17:29:22 +0900

    Diet ID changed

--- src/main/java/ewhamenu/com/demo/domain/Diet.java
+++ src/main/java/ewhamenu/com/demo/domain/Diet.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Diet {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     @Column(name = "dietId")
     private long dietId;
     @Column(name = "createdate")
